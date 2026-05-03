34ed4d4cc63d61471dc49cb0ea4feea8ee43dc05
Noor Ul Huda <huda.noor.network@gmail.com>
2026-05-04 00:17:56 +0500

    data_types.py

---
+++ 01_basics/data_types.py
@@ -0,0 +1,18 @@
+#===================
+# DATA TYPES
+#===================
+name = "faizan" #string
+age = 21 #integer
+leaves = 4.5 #float
+is_student = True #boolean
+fruits = ["apple", "mango", "banana", "orange"] #list
+fruits = ("apple", "mango", "banana", "orange") #tuple 
+num = {1, 3, 5, 7, 9} #set
+num1 = ({1, 3, 5, 7, 9}) #frozen set 
+range = range(1) #range
+a = () # none
+#Dictionary
+address = {   
+    "street": "15th Street",
+    "area": "DHA 5"
+}
