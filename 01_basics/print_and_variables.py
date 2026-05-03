116b29dc5bc27d0f02d82b802dcdf0b32fd2ecb0
Noor Ul Huda <huda.noor.network@gmail.com>
2026-05-03 23:34:48 +0500

    print_and_variables.py

---
+++ 01_basics/print_and_variables.py
@@ -0,0 +1,40 @@
+# =======================
+# BASIC PRINT STATEMENTS
+# =======================
+# This is how we print some basic statements in python.
+print("Hello World")
+print(21)
+print("My Name is Noor.", "I'm 30 years old", "and I want to learn Python.")
+print(24+2)
+print("24+2")
+# =======================
+# COMMENTS IN PYTHON
+# =======================
+# comments are non-executable lines used to explain code logic.
+# I'm a Single line Comment.
+"""
+I'ma Milti line Comment.
+I'm written in the 3 inverter commas.
+"""
+# =======================
+# VARIABLES
+# =======================
+name = 'Noor'
+age = 30 
+print(name, age)
+print ("My name is:", name)
+
+age =  32
+future_age = age + 1
+f_age = future_age
+print("I'm going to be", f_age, "years old next year.")
+
+print(type(name))
+print(type(age))
+print(type(future_age))
+
+# Addition
+a = 10
+b = 5
+add = a + b
+print("The sum of a and b is:", add)
